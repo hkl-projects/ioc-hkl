6672a439f237a28265c90f2e837b5bbebbf88aa7
Alex-dt <alexanderbaekey@gmail.com>
2025-11-13 21:42:13 -0500

    fixed building with pixi python environment, now entirely use system python or environment python

---
+++ iocBoot/iocpydev/st_sys.cmd
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export GI_TYPELIB_PATH=/usr/local/lib/girepository-1.0
+# export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+which python3
+
+../../bin/linux-x86_64/hklApp st_base.cmd
+
